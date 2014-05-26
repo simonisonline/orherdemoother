4fb03fee512997d973de5bc265f072e252b87383
simon <info@simonisonline.nl>
2014-05-26 23:08:41 +0200

    D

---
+++ folders/d.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of d
+ *
+ * @author simon
+ */
+class d {
+    protected $d;
+    
+    public function setD($string){
+        $this->d = $string;
+        return $this;
+    }
+    
+    public function getD(){
+        return $this->d;
+    }
+}
